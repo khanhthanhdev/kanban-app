9b727570a6e5e170e6fe8fb1073495cc77d3a370
khanhthanhdev <thanhkt27507@gmail.com>
2023-06-22 10:46:40 +0700

    add origin: * in app.js

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -13,8 +13,8 @@ axiosClient.interceptors.request.use(async config => {
   return {
     ...config,
     headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+      // "Access-Control-Allow-Origin": "*",
+      // "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
       'Content-Type': 'application/json',
       'authorization': `Bearer ${getToken()}`
     }
